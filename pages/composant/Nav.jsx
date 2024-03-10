bac2be6eb5c5f6ee0ff21b3cab3c8bf82d01d6ef
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-03-10 12:40:32 +0300

    Tero amzay ikaly aloa haha

--- pages/composant/Nav.jsx
+++ pages/composant/Nav.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='head'>
+    <div class="header-title">
         <header>
             <h1 className='logo'>LOGO</h1>
             <nav>
-                <ul>
+                <ul className="list-nav">
                     <li className='inline-block'><a href="/pages/accueil">Accueil</a></li>
                     <li className='inline-block'><a href="/pages/position">Position</a></li>
                     <li className='inline-block'><a href="">FAQ</a></li>
